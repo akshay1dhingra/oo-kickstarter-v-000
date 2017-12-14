17dc5046946d1f48ed361b3b807433b3d58f41a8
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-14 16:50:45 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
 
   attr_accessor :name
 
-  backed_projects = []
+  @@backed_projects = []
 
   def initialize(name)
     @name = name
@@ -10,7 +10,7 @@ class Backer
 
   def back_project(title)
     @title = title
-    @backed_projects << title
+    @@backed_projects << title
   end
 
 
