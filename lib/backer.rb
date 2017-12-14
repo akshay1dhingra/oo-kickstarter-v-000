7da9bfa3714d8ae1d5b2631c7f6f410448168f8e
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-14 16:53:49 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,8 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(title)
-    @title = title
-    @backed_projects << title
+  def back_project(project)
+    @backed_projects << project
   end
 
 
